f65d65e7a124b620cb2ecd7d7634b305c2ec6e4a
Tony Cheng <soar1229@gmail.com>
2017-03-20 13:36:05 +0800

    建立第一個TestCase

---
+++ BMICalculator/BMIService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BMICalculator
+{
+    public class Person
+    {
+        public double Weight { get; set; }
+
+        public double Height { get; set; }
+
+        public string CalculateBMI()
+        {
+            return "";
+        }
+    }
+}
---
+++ UnitTestForBMICalc/UnitTestBMICalculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BMICalculator;
+
+namespace UnitTestForBMICalc
+{
+    [TestClass]
+    public class UnitTestBMICalculator
+    {
+        [TestMethod]
+        public void Input_BMI_19_Output_太瘦()
+        {
+            //Arrange
+            var expect = "太瘦";
+
+            //Act
+            var actual = new Person().CalculateBMI();
+
+            //Assert
+            Assert.AreEqual(expect, actual);
+        }
+    }
+}
