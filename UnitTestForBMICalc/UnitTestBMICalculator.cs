00f94fd249ef3b065c875286d5e489809d321ea0
Tony Cheng <soar1229@gmail.com>
2017-03-20 13:54:10 +0800

    新增第二個TestCase

--- UnitTestForBMICalc/UnitTestBMICalculator.cs
+++ UnitTestForBMICalc/UnitTestBMICalculator.cs
@@ -21,5 +21,20 @@ namespace UnitTestForBMICalc
             //Assert
             Assert.AreEqual(expect, actual);
         }
+
+        [TestMethod]
+        public void Input_Person_Height_170CM_Weight_80KG_Output_太胖()
+        {
+            //Arrange
+            var expect = "太胖";
+
+            var person = new Person.Person(170, 80);
+
+            //Act
+            var actual = person.CalculateBMI();
+
+            //Assert
+            Assert.AreEqual(expect, actual);
+        }
     }
 }
