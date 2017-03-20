806bd6fe0ea3b848c994f201f7cabd555cc3e197
Tony Cheng <soar1229@gmail.com>
2017-03-20 14:45:45 +0800

    新增三個Test Cases For Women

--- UnitTestForBMICalc/UnitTestBMICalculator.cs
+++ UnitTestForBMICalc/UnitTestBMICalculator.cs
@@ -51,5 +51,50 @@ namespace UnitTestForBMICalc
             //Assert
             Assert.AreEqual(expect, actual);
         }
+
+        [TestMethod]
+        public void Input_Women_Height_160CM_Weight_40KG_Output_太瘦()
+        {
+            //Arrange
+            var expect = "太瘦";
+
+            var person = new Women(160, 50);
+
+            //Act
+            var actual = person.CalculateBMI();
+
+            //Assert
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void Input_Women_Height_160CM_Weight_90KG_Output_太胖()
+        {
+            //Arrange
+            var expect = "太胖";
+
+            var person = new Women(160, 90);
+
+            //Act
+            var actual = person.CalculateBMI();
+
+            //Assert
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void Input_Women_Height_160CM_Weight_50KG_Output_適中()
+        {
+            //Arrange
+            var expect = "適中";
+
+            var person = new Women(160, 50);
+
+            //Act
+            var actual = person.CalculateBMI();
+
+            //Assert
+            Assert.AreEqual(expect, actual);
+        }
     }
 }
