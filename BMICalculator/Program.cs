d766928232d2c1c84300b98a005f29555aeabf79
TONY <TONY@15000651TONY>
2017-03-20 09:07:46 +0800

    Add project files.

---
+++ BMICalculator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BMICalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
